14def1cd5671b608a1767c1eb7946cb954c50b44
Victor Leyva <vic.leyva5@gmail.com>
2024-02-13 20:41:27 -0600

    Visual fix changes for NoNotesFound

--- CloneKeepApp/src/components/AppWrapper/NoNotesFound.js
+++ CloneKeepApp/src/components/AppWrapper/NoNotesFound.js
@@ -5,8 +5,8 @@ import { Typography, Paper } from '@mui/material';
 export default function NoNotesFound() {
     return (
         <Paper sx={{ padding: '1rem', marginTop: '2rem', textAlign: 'center', width: '100%' }}>
-            <Typography variant="h6">No Notes Found</Typography>
-            <Typography variant="body2">Try changing your search criteria or create a new note.</Typography>
+            <Typography variant="h2">No Notes Found</Typography>
+            <Typography variant="body1">Try changing your search criteria or create a new note.</Typography>
         </Paper>
     );
 }
